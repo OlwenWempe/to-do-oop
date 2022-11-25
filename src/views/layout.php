a7c0301df66c9659d0ee620a2345d1ae868a59a6
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-25 19:03:17 +0100

    changed layout

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,16 +1,44 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title><?= $title ?></title>
 </head>
-<body>
 
-        <h1>Ceci est la vue</h1>
-        <?= $content ?>
+<body id="body">
+
+    <h1>Ceci est la vue</h1>
+    <?= $content ?>
+
+
+
+    <footer class=" bg-dark">
+        <ul class="nav justify-content-center py-3">
+            <li class="nav-item">
+                <a class="nav-link" href="#" aria-current="page">CGU</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link color-light" href="#">politique de confidentialité</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link color-light" href="https://owempe.eu">@copyright Olwen WEMPE</a>
+            </li>
+        </ul>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 
 </body>
+
+</html>
+</body>
+
 </html>
